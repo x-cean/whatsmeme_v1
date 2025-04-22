45b1b7183d35d8e828f9ba8df65660e2b6a23738
Eli_Nor <elinor.kotzott@gmx.de>
2025-04-22 19:07:10 +0200

    working on ai meme creation

--- ai/openai_helper.py
+++ ai/openai_helper.py
@@ -1,4 +1,3 @@
-
 import os
 import openai
 from dotenv import load_dotenv
@@ -6,6 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv("OPEN_AI_KEY")
+
+def get_generated_meme_from_openai(prompt):
+    try:
+        response = openai.Image.create(prompt=prompt, n=1, size="512x512")
+        print(response)
+        meme_url = response['data'][0]['url']
+        return meme_url
+    except openai.OpenAIError as e:
+        print(f"An error occurred: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #hello - elinor
 #hi - Xiao
 #just another test - X
+
+from ai import openai_helper as ai
+from data.datamanager import send_message
+
+prompt = "create a cat meme, please. it should be cute"
+
+meme_url = ai.get_generated_meme_from_openai(prompt)
+if meme_url:
+    send_message(meme_url)
