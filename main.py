15f2908eb04e2344f9c036e2708d7fe5f9e9181b
Eli_Nor <elinor.kotzott@gmx.de>
2025-04-23 21:14:54 +0200

    added working code to main to be able to show something

--- main.py
+++ main.py
@@ -11,41 +11,49 @@ twilio_number = os.getenv("TWILIO_PHONE_NUMBER")
 user_number = os.getenv("USER_PHONE_NUMBER")
 
 
+#AI generation of pictures works
+prompt = "A funny cat meme in cartoon style. The cat in the picture should be orange"
+send_msg_with_media(twilio_number, user_number, "Here is your picture!", get_generated_meme_from_openai(prompt))
 
-while True:
-    chat_ids = get_conversation_sids()
-    print(chat_ids)
-
-
-    for chat_id in chat_ids:
-        print(chat_id)
-        #welcome_user(chat_id) this should send a welcome message based on whether the user already exists
-
-        send_meme_via_whatsapp(twilio_number, user_number) #sends top meme of the day
-
-        latest_messages = retrieve_latest_message()
+#sending top meme of the day with AI comment works
+send_meme_via_whatsapp(twilio_number, user_number)
 
 
 
 
-        if latest_messages:
-            if latest_messages[0] == 1:
-                # random meme will be picked and sent
-                pass
-            elif latest_messages[0] == 2:
-                # choosing a topic for the meme, fitting meme will be sent
-                pass
-            elif latest_messages[0] == 3:
-                send_text_message("What kind of meme should it be? Give me a short description!")
-                while True:
-                    latest_messages = retrieve_latest_message()
-                    if latest_messages[0] == 3:
-                        continue
-                    prompt = latest_messages[0]
-                    send_msg_with_media(twilio_number, user_number, "Here is your AI generated picture!", get_generated_meme_from_openai(prompt))
-                    break
-
-
+# while True:
+#
+#     chat_ids = get_conversation_sids()
+#     print(chat_ids)
+#
+#
+#     for chat_id in chat_ids:
+#         print(chat_id)
+#         #welcome_user(chat_id) this should send a welcome message based on whether the user already exists
+#
+#         send_meme_via_whatsapp(twilio_number, user_number) #sends top meme of the day
+#
+#         latest_messages = retrieve_latest_message()
+#
+#
+#         if latest_messages:
+#             if latest_messages[0] == 1:
+#                 # random meme will be picked and sent
+#                 pass
+#             elif latest_messages[0] == 2:
+#                 # choosing a topic for the meme, fitting meme will be sent
+#                 pass
+#             elif latest_messages[0] == 3:
+#                 send_text_message("What kind of meme should it be? Give me a short description!")
+#                 while True:
+#                     latest_messages = retrieve_latest_message()
+#                     if latest_messages[0] == 3:
+#                         continue
+#                     prompt = latest_messages[0]
+#                     send_msg_with_media(twilio_number, user_number, "Here is your AI generated picture!", get_generated_meme_from_openai(prompt))
+#                     break
+#
+#
 
 
 
